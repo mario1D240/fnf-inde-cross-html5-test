cb2176586664731dfc1a8d24f97d615b890052b8
Sans <83004239+mario1D240@users.noreply.github.com>
2022-04-20 14:17:52 +0800

    Update script.lua

--- assets/preload/data/cocoa/script.lua
+++ assets/preload/data/cocoa/script.lua
@@ -1,7 +1,7 @@
 local allowCountdown = false
 function onStartCountdown()
 	if not allowCountdown and isStoryMode and not seenCutscene then --Block the first countdown
-		startVideo('1');
+		startVideo('Test');
 		allowCountdown = true;
 		return Function_Stop;
 	end
