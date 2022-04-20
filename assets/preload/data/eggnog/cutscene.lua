1702d028b89ec8789fd3f4825b8e1ba29bbbee15
Sans <83004239+mario1D240@users.noreply.github.com>
2022-04-20 14:18:21 +0800

    Delete cutscene.lua

--- assets/preload/data/eggnog/cutscene.lua
+++
@@ -1,9 +0,0 @@
-local allowCountdown=false;
-function onStartCountdown()
-  if not allowCountdown and isStoryMode and not seenCutscene then
-    startVideo('2');
-    allowCountdown=true;
-    return Function_Stop;
-  end
-  return Function_Continue;
-  end
