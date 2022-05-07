9b6d68e589d2b703e195315e5e26888471dca3fa
Sans <83004239+mario1D240@users.noreply.github.com>
2022-05-07 10:58:05 +0800

    Delete script.lua

--- assets/preload/data/bopeebo/script.lua
+++
@@ -1,8 +0,0 @@
-function onStartCountdown()
-	if not allowCountdown and isStoryMode and not seenCutscene then --Block the first countdown
-		startVideo('test');
-		allowCountdown = true;
-		return Function_Stop;
-	end
-	return Function_Continue;
-end
